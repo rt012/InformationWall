3f3f6a828c41dca3f3e1f3105a9647bd7524c14a
Eric Schmidt <eric.schmidt93@gmail.com>
2015-11-21 13:50:48 +0100

    Fix RealPathHelper

--- InformationWallAndroidApp/app/src/main/java/wipraktikum/informationwallandroidapp/Utils/RealPathHelper.java
+++ InformationWallAndroidApp/app/src/main/java/wipraktikum/informationwallandroidapp/Utils/RealPathHelper.java
@@ -4,10 +4,12 @@ package wipraktikum.informationwallandroidapp.Utils;
  * Created by Eric Schmidt on 01.11.2015.
  */
 
-import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 
@@ -31,102 +33,144 @@ public class RealPathHelper{
     }
 
     public String getRealPathFromURI(Uri data){
-        String realPath;
+        String realPath = null;
 
         File file = new File(data.getPath());
+
         if (FileHelper.getInstance().exists(file.getAbsolutePath())){
             realPath = file.getAbsolutePath();
         }else {
-            //There are three cases (up till now): audio, video, otherwise (Documents and Images)
-            String[] mediaType = data.getPath().split("/"); //Get to the content type + ID
-            mediaType = mediaType[mediaType.length-1].split(":"); //Get tot the content type
-            switch (mediaType[0]){
-                case "audio":
-                    realPath = getRealPathFromURIAudio(data);
-                    break;
-                case "video":
-                    realPath = getRealPathFromURIVideo(data);
-                    break;
-                default:
-                    realPath = getRealPathFromURIDocument(data);
-                    break;
-            }
+            realPath = getRealPathFromUri(data);
         }
         return realPath;
     }
 
-    @SuppressLint("NewApi")
-    private String getRealPathFromURIDocument(Uri uri){
-        String filePath = "";
-        String wholeID = DocumentsContract.getDocumentId(uri);
+    /**
+     * Get a file path from a Uri. This will get the the path for Storage Access
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     *
+     * @param uri The Uri to query.
+     * @author paulburke @https://gist.github.com/prasad321/9852037
+     */
+    public String getRealPathFromUri(final Uri uri) {
 
-        // Split at colon, use second item in the array
-        String id = wholeID.split(":")[1];
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
 
-        String[] column = { MediaStore.Images.Media.DATA };
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(mContext, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
 
-        // where id is equal to
-        String sel = MediaStore.Images.Media._ID + "=?";
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
 
-        Cursor cursor = mContext.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                column, sel, new String[]{ id }, null);
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
 
-        int columnIndex = cursor.getColumnIndex(column[0]);
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
 
-        if (cursor.moveToFirst()) {
-            filePath = cursor.getString(columnIndex);
+                return getDataColumn(mContext, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(mContext, contentUri, selection, selectionArgs);
+            }
         }
-        cursor.close();
-        return filePath;
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(mContext, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
     }
 
-    @SuppressLint("NewApi")
-    private String getRealPathFromURIVideo(Uri uri){
-        String filePath = "";
-        String wholeID = DocumentsContract.getDocumentId(uri);
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @param selection (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
 
-        // Split at colon, use second item in the array
-        String id = wholeID.split(":")[1];
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
 
-        String[] column = { MediaStore.Video.Media.DATA };
-
-        // where id is equal to
-        String sel = MediaStore.Video.Media._ID + "=?";
-
-        Cursor cursor = mContext.getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                column, sel, new String[]{ id }, null);
-
-        int columnIndex = cursor.getColumnIndex(column[0]);
-
-        if (cursor.moveToFirst()) {
-            filePath = cursor.getString(columnIndex);
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
         }
-        cursor.close();
-        return filePath;
+        return null;
     }
 
-    @SuppressLint("NewApi")
-    private String getRealPathFromURIAudio(Uri uri){
-        String filePath = "";
-        String wholeID = DocumentsContract.getDocumentId(uri);
 
-        // Split at colon, use second item in the array
-        String id = wholeID.split(":")[1];
-
-        String[] column = { MediaStore.Audio.Media.DATA };
-
-        // where id is equal to
-        String sel = MediaStore.Audio.Media._ID + "=?";
-
-        Cursor cursor = mContext.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                column, sel, new String[]{ id }, null);
-
-        int columnIndex = cursor.getColumnIndex(column[0]);
-
-        if (cursor.moveToFirst()) {
-            filePath = cursor.getString(columnIndex);
-        }
-        cursor.close();
-        return filePath;
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
     }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
 }
